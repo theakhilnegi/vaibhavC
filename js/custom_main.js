95a26743f55e480c7e1ed04fc62d45076d85163c
theakhilnegi <akhilnegi3111@gmail.com>
2023-03-25 03:46:32 +0530

    first commit

---
+++ js/custom_main.js
@@ -0,0 +1,190 @@
+// toggle
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+  });
+
+
+
+// Search
+  $(document).ready(function(){
+    $("#search").focus(function() {
+      $(".search-box").addClass("border-searching");
+      $(".search-icon").addClass("si-rotate");
+    });
+    $("#search").blur(function() {
+      $(".search-box").removeClass("border-searching");
+      $(".search-icon").removeClass("si-rotate");
+    });
+    $("#search").keyup(function() {
+        if($(this).val().length > 0) {
+          $(".go-icon").addClass("go-in");
+        }
+        else {
+          $(".go-icon").removeClass("go-in");
+        }
+    });
+    $(".go-icon").click(function(){
+      $(".search-form").submit();
+    });
+});
+
+
+
+
+// scroll bar 
+(function() {
+
+  var indicator = $('#indicator');
+  var counter = indicator.find('span');
+
+  var win = jQuery(window);
+  if (indicator.length) {
+    var moveIndicator = debounce(function() {
+      var viewportHeight = $(window).height();
+      var documentHeight = $(document).height();
+      var hasScrolled = $(window).scrollTop();
+
+      var percent = (hasScrolled / (documentHeight - viewportHeight)) * 100;
+      indicator.css("top", percent + "%");
+      counter.html(Math.floor(percent) + "%");
+
+    }, 10);
+
+  }
+
+  win.on("resize scroll", moveIndicator);
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function() {
+      var context = this,
+        args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+}());
+
+
+
+// gallary
+$(document).ready(function(){
+
+	[...document.querySelectorAll('.first-column')].map(column => {
+	column.style.setProperty('--animation', 'slide');
+	column.style.setProperty('height', '200%');
+	column.innerHTML = column.innerHTML + column.innerHTML;
+	});
+	
+	});
+
+
+
+
+
+
+
+
+
+/* Please ❤ this if you like it! */
+
+
+
+(function($) { "use strict";
+
+	//Switch dark/light
+	
+	$(".switch").on('click', function () {
+		if ($("body").hasClass("light")) {
+			$("body").removeClass("light");
+			$(".switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("light");
+			$(".switch").addClass("switched");
+		}
+	});
+		
+	$(document).ready(function(){"use strict";
+	
+		//Scroll back to top
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 550;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		
+	});
+	
+})(jQuery); 
+
+
+
+
+
+
+
+
+
+// me phtot 
+
+const els = document.querySelectorAll("[type='radio']");
+for (const el of els)
+  el.addEventListener("input", e => reorder(e.target, els));
+reorder(els[0], els);
+
+function reorder(targetEl, els) {
+  const nItems = els.length;
+  let processedUncheck = 0;
+  for (const el of els) {
+    const containerEl = el.nextElementSibling;
+    if (el === targetEl) {//checked radio
+      containerEl.style.setProperty("--w", "100%");
+      containerEl.style.setProperty("--l", "0");
+    }
+    else {//unchecked radios
+      containerEl.style.setProperty("--w", `${100/(nItems-1)}%`);
+      containerEl.style.setProperty("--l", `${processedUncheck * 100/(nItems-1)}%`);
+      processedUncheck += 1;
+    }
+  }
+}
+
